b51cfa0aacd70019b8fcd8bd2e222612fea5af9d
Ranithar12 <158450530+Ranithar12@users.noreply.github.com>
2024-04-05 10:39:12 +0530

    forloop7.sh

---
+++ forloop7.sh
@@ -0,0 +1,16 @@
+//fetch files from specific file extension
+#!/bin/bash
+for i in *.html
+do 
+echo $i
+sleep2
+done
+
+
+//fetch files from specific directory
+#!/bin/bash
+for i in /etc/*.conf
+do 
+echo $i
+sleep2
+done
